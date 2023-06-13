8e4dc6c17e6db8319a0994efb9b2905c12b7c946
hanntoo <reihansan@gmail.com>
2023-06-13 18:20:34 +0700

    mengedit database dan navbar pembeli

--- database/migrations/2023_06_04_034311_create_produk_table.php
+++ database/migrations/2023_06_04_034311_create_produk_table.php
@@ -18,16 +18,20 @@ class CreateProdukTable extends Migration
             $table->unsignedBigInteger('id_penjual');
             $table->unsignedBigInteger('id_kategori');
             $table->string('nama_produk', 100);
-            $table->integer('harga_produk', 50);
+            $table->unsignedBigInteger('harga_produk');
             $table->string('berat_produk', 255);
             $table->string('foto_produk', 255);
             $table->text('deskripsi_produk');
-            $table->integer('stok_produk', 20);
+            $table->unsignedInteger('stok_produk');
             $table->timestamps();
 
             // Add the foreign key constraints
             $table->foreign('id_penjual')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('id_kategori')->references('id')->on('kategori')->onDelete('cascade');
+
+            // Add unique constraints
+            $table->unique('harga_produk');
+            $table->unique('stok_produk');
         });
     }
 
@@ -42,6 +46,10 @@ class CreateProdukTable extends Migration
             // Drop the foreign key constraints
             $table->dropForeign(['id_penjual']);
             $table->dropForeign(['id_kategori']);
+
+            // Drop the unique constraints
+            $table->dropUnique(['harga_produk']);
+            $table->dropUnique(['stok_produk']);
         });
 
         Schema::dropIfExists('produk');
