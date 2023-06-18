fc9991d1a93a3b1513295fd66d889529efa584e3
Fa_Skyz <luthfaismusyaffa@gmail.com>
2023-06-18 14:07:02 +0700

    menambahkan halaman about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,94 @@
 <x-app-layout>
+<x-slot name="header">
+<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('About Us') }}
+        </h2>
+</x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    About Page!
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="card profile-card-3">
+                                    <div class="background-block">
+                                        <img src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="profile-sample1" class="background"/>
+                                    </div>
+                                    <div class="profile-thumb-block">
+                                        <img src="https://randomuser.me/api/portraits/men/41.jpg" alt="profile-image" class="profile"/>
+                                    </div>
+                                    <div class="card-content">
+                                        <h1>Luth Fais Musyaffa<br><small>Front End Developer</small></h1>
+                                        <div class="icon-block">
+                                            <a href="https://github.com/luthfaismusyaffa"><i class="fa-brands fa-github"></i></a>
+                                            <a href="https://www.linkedin.com/in/luth-fais-musyaffa-452829270/"><i class="fa-brands fa-linkedin"></i></a>
+                                            <a href="https://instagram.com/e.fyoimsky?igshid=MzNlNGNkZWQ4Mg=="><i class="fa-brands fa-instagram"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-3">
+                                <div class="card profile-card-3">
+                                    <div class="background-block">
+                                        <img src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="profile-sample1" class="background"/>
+                                    </div>
+                                    <div class="profile-thumb-block">
+                                        <img src="https://randomuser.me/api/portraits/men/41.jpg" alt="profile-image" class="profile"/>
+                                    </div>
+                                    <div class="card-content">
+                                        <h1>Reihan Ramadhan<br><small>Front-End Developer</small></h1>
+                                        <div class="icon-block">
+                                            <a href="https://github.com/hanntoo"><i class="fa-brands fa-github"></i></a>
+                                            <a href="https://www.linkedin.com/in/reihan-ramadhan-hann109"><i class="fa-brands fa-linkedin"></i></a>
+                                            <a href="https://instagram.com/reihansan"><i class="fa-brands fa-instagram"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-3">
+  <div class="card profile-card-3">
+    <div class="background-block">
+      <img src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="profile-sample1" class="background"/>
+    </div>
+    <div class="profile-thumb-block">
+      <img src="https://randomuser.me/api/portraits/men/41.jpg" alt="profile-image" class="profile"/>
+    </div>
+    <div class="card-content">
+      <h1 style="white-space: nowrap;">Andreas Rona Nugraha<br><small>Back-End Developer</small></h1>
+      <div class="icon-block">
+      <a href="https://github.com/genshinshare"><i class="fa-brands fa-github"></i></a>
+      <a href="https://www.linkedin.com/in/andreasrn/ "><i class="fa-brands fa-linkedin"></i></a>
+      <a href="https://instagram.com/andreas_rn"><i class="fa-brands fa-instagram"></i></a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="col-md-3">
+  <div class="card profile-card-3">
+    <div class="background-block">
+      <img src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="profile-sample1" class="background"/>
+    </div>
+    <div class="profile-thumb-block">
+      <img src="https://randomuser.me/api/portraits/men/41.jpg" alt="profile-image" class="profile"/>
+    </div>
+    <div class="card-content">
+      <h1 style="white-space: nowrap;">La Ode Ridho Fahreza<br><small>Full-Stack Developer</small></h1>
+      <div class="icon-block">
+      <a href="https://github.com/laoderidho"><i class="fa-brands fa-github"></i></a>
+      <a href="https://www.linkedin.com/in/la-ode-ridho-fahreza-99032b248/"><i class="fa-brands fa-linkedin"></i></a>
+      <a href="https://www.instagram.com/laode_ridho_fahreza/"><i class="fa-brands fa-instagram"></i></a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
